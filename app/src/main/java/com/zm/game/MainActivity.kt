a43fb35165ffa3eaac04170bd9444507d3738096
zhangzi <zhangmin9x9@gmail.com>
2023-11-15 22:00:00 +0800

    首次初始化

--- app/src/main/java/com/zm/game/MainActivity.kt
+++ app/src/main/java/com/zm/game/MainActivity.kt
@@ -23,11 +23,9 @@ class MainActivity : AppCompatActivity() {
     private val playerView: PlayerView by lazy { findViewById<PlayerView>(R.id.player_view) }
     private val ivCover: ImageView by lazy { findViewById<ImageView>(R.id.ivCover) }
     private val bt: Button by lazy { findViewById<Button>(R.id.bt) }
+    private val bt2: FloatingActionButton by lazy { findViewById<FloatingActionButton>(R.id.bt2) }
 
-    lateinit var mPlayer: ExoPlayer
-    val playUrl = "android.resource://" + packageName + "/" + R.raw.palying
-    val playUrl2 = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
-    val m1 by lazy { MediaItem.fromUri(Uri.parse(playUrl)) }
+    private lateinit var mPlayer: ExoPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fullscreen)
@@ -36,20 +34,59 @@ class MainActivity : AppCompatActivity() {
         }
         playerView.player = mPlayer
         mPlayer.setPlaybackSpeed(0.5f)
-        mPlayer.setMediaItem(m1)
-        mPlayer.prepare()
+
+
+        val playUrl0 = "android.resource://" + "/" + R.raw.p0
+        mPlayer.addMediaItem(MediaItem.fromUri(Uri.parse(playUrl0)))
+        val playUrl1 = "android.resource://" + "/" + R.raw.p1
+        mPlayer.addMediaItem(MediaItem.fromUri(Uri.parse(playUrl1)))
+        val playUrl2 = "android.resource://" + "/" + R.raw.p2
+        mPlayer.addMediaItem(MediaItem.fromUri(Uri.parse(playUrl2)))
+        val playUrl3 = "android.resource://" + "/" + R.raw.p3
+        mPlayer.addMediaItem(MediaItem.fromUri(Uri.parse(playUrl3)))
+        val playUrl4 = "android.resource://" + "/" + R.raw.p4
+        mPlayer.addMediaItem(MediaItem.fromUri(Uri.parse(playUrl4)))
+
         ivCover.visibility = View.VISIBLE
+        mPlayer.prepare()
 
         bt.setOnClickListener {
             if (mPlayer.isPlaying) {
                 mPlayer.pause()
                 mPlayer.seekTo(0)
                 bt.text = "开"
+                when (mPlayer.currentMediaItemIndex) {
+                    0 -> ivCover.setImageResource(R.mipmap.g0)
+                    1 -> ivCover.setImageResource(R.mipmap.g1)
+                    2 -> ivCover.setImageResource(R.mipmap.g2)
+                    3 -> ivCover.setImageResource(R.mipmap.g3)
+                    4 -> ivCover.setImageResource(R.mipmap.g4)
+                }
+
                 ivCover.visibility = View.VISIBLE
             } else {
                 mPlayer.play()
                 bt.text = "关"
                 ivCover.visibility = View.GONE
+            }
+        }
+
+        bt2.setOnClickListener {
+            if (mPlayer.isPlaying) {
+                mPlayer.pause()
+                bt.text = "开"
+            }
+            if (mPlayer.hasNextMediaItem()) {
+                mPlayer.seekToNextMediaItem()
+            }else{
+                mPlayer.seekToDefaultPosition(0)
+            }
+            when (mPlayer.currentMediaItemIndex) {
+                0 -> ivCover.setImageResource(R.mipmap.g0)
+                1 -> ivCover.setImageResource(R.mipmap.g1)
+                2 -> ivCover.setImageResource(R.mipmap.g2)
+                3 -> ivCover.setImageResource(R.mipmap.g3)
+                4 -> ivCover.setImageResource(R.mipmap.g4)
             }
         }
     }
